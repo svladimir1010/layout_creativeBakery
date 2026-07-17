11b5d1dec29455e3ae517bf5cefed6c573d5e5af
svladimir1010 <vladimireur@gmail.com>
2026-07-17 23:14:52 +0300

    feat: fix font-family quotes for stylelint

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+import '../styles/main.scss';
+// ─── mob-menu Bakerlab ───────────────────────────────────────────────
+
+const burgerBtn = document.querySelector('.header__burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBtn = document.querySelector('.mobile-menu__close');
+const menuElements = document.querySelectorAll(
+  '.mobile-menu__link, .mobile-menu__contact',
+);
+
+const toggleMenu = () => {
+  mobileMenu.classList.toggle('mobile-menu--active');
+  document.documentElement.classList.toggle('no-scroll');
+};
+
+if (burgerBtn) {
+  burgerBtn.addEventListener('click', toggleMenu);
+}
+
+if (closeBtn) {
+  closeBtn.addEventListener('click', toggleMenu);
+}
+
+menuElements.forEach((element) => {
+  element.addEventListener('click', toggleMenu);
+});
